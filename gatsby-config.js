f892641213c03538ef20358074fc47ad112f784c
Tom Walsh <hello@tmwlsh.co.uk>
2021-11-18 13:45:39 +0000

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "PB Joinery",
+  },
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet"],
+};
