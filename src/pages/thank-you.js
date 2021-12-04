97a7f7f8a395c1a91ea020358237ec3c99cb3dbc
Tom Walsh <hello@tmwlsh.co.uk>
2021-12-04 11:39:53 +0000

    feat: adds thank you page

---
+++ src/pages/thank-you.js
@@ -0,0 +1,13 @@
+import * as React from "react";
+import Layout from "../components/layout";
+
+const ThankYou = () => {
+
+  return (
+    <Layout>
+      <h1>Thank you</h1>
+    </Layout>
+  )
+};
+
+export default ThankYou;
